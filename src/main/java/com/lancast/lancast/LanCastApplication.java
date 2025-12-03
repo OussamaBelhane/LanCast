c53bc6ed06b6a6f0f0b8ec637b954fee4222b68a
invictus <oussama.belhane@gmail.com>
2025-12-03 13:16:31 +0100

    Update

--- src/main/java/com/lancast/lancast/LanCastApplication.java
+++ src/main/java/com/lancast/lancast/LanCastApplication.java
@@ -11,9 +11,10 @@ public class LanCastApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(LanCastApplication.class.getResource("lancast-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 900, 600);
         stage.setTitle("LanCast");
         stage.setScene(scene);
         stage.show();
+        stage.centerOnScreen();
     }
 }
